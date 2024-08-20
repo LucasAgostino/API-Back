db50467747ef54525cb7f6340e06b8a87a82d944
Lucas <luagostino@uade.edu.ar>
2024-08-20 18:06:01 -0300

    Creacion de funcion que trae todos los productos

--- api/src/main/java/com/api/api/controller/ProductoController.java
+++ api/src/main/java/com/api/api/controller/ProductoController.java
@@ -1,11 +1,15 @@
 package com.api.api.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.api.dominio.Producto;
 import com.api.api.service.ProductoService;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -19,6 +23,11 @@ public class ProductoController {
     @PostMapping("/create")
     public Producto postMethodName(@RequestBody Producto producto) { 
         return productoService.crearProducto(producto);
+    }
+    
+    @GetMapping("/get")
+    public List<Producto> postMethodName() {
+        return productoService.getProducto();
     }
     
 }
--- api/src/main/java/com/api/api/service/ProductoService.java
+++ api/src/main/java/com/api/api/service/ProductoService.java
@@ -1,5 +1,7 @@
 package com.api.api.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,4 +34,10 @@ public class ProductoService {
 
         return productoRepository.save(producto);
     }
+
+    public List<Producto> getProducto() {
+        List<Producto> productos = productoRepository.findAll();
+        return productos;
+    }
+    
 }
